6461cb8af88566a709141755cc8eb42e50e9ca31
surajaswal29 <surajaswal29@gmail.com>
2024-01-17 18:50:18 +0530

    DApp practice

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,13 +1,78 @@
 import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
 
 actor DBank {
-  var myBank = "My bank name is xyz";
+  // current time
+  stable var start_time : Time.Time = Time.now();
+  let user_id : Text = "7c2r27383bc3rcb832873c8";
+  let avail_bank : Text = "Abc bank limited";
 
-  var myBalance = 100000;
+  Debug.print(debug_show(start_time / 1000000000));
 
-  let myAccountNumber = 3732737832783426;
+  stable var avail_balance : Float = 1000;
 
-  Debug.print(myBank);
-  Debug.print(debug_show(myBalance));
-  Debug.print(debug_show(myAccountNumber));
+  // avail_balance := 20100;
+
+  var user : Text = "Suraj Aswal";
+
+  let avail_account = 1234567890;
+
+  Debug.print(debug_show({
+    user_id = user_id;
+    user = user;
+    bank = avail_bank;
+    balance = avail_balance;
+    account = avail_account
+    }));
+
+
+  public func updateUserBalance(value: Float) : async() {
+    avail_balance += value;
+
+    Debug.print(debug_show({
+        message = "Amount added successfully";
+        amount_added = value;
+        final_balance = avail_balance;
+    }));
+  };
+
+  public func withdrawUserBalance(value: Float) : async() {
+
+    let temp_value : Float = avail_balance - value;
+
+    if(temp_value >= 0){
+      avail_balance -= value;
+
+      Debug.print(debug_show({
+          message = "Amount withdrawn successfully";
+          amount_withdrawn = value;
+          final_balance = avail_balance;
+      }));
+    }else{
+      Debug.print(debug_show({
+        message = "Amount too large. Available balance is lower than the entered amount"
+      }))
+    }
+
+      
+  };
+
+  public query func checkBalance() : async Float{
+    Debug.print(debug_show(avail_balance));
+    return avail_balance;
+  };
+
+
+  public func calCompoundInterest(){
+    let current_time = Time.now();
+    let time_elapsed_NS = current_time - start_time;
+    let time_elapsed_S = time_elapsed_NS / 1000000000;
+
+    avail_balance := avail_balance * (1.01 ** Float.fromInt(time_elapsed_S));
+
+    start_time := current_time;
+  }
+
+  // updateUserBalance();
 }
--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,19 +1,57 @@
-import { dbank_backend } from "../../declarations/dbank_backend";
+import {dbank_backend} from "../../declarations/dbank_backend"
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+let loading = false;
 
-  const name = document.getElementById("name").value.toString();
+// loading current available balance whenever the application loads
+window.addEventListener("load", async ()=>{
+  const currentBalance = await dbank_backend.checkBalance();
 
-  button.setAttribute("disabled", true);
+  document.getElementById("current-balance-amount").innerText = Math.round(currentBalance * 100) / 100;
+})
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank_backend.greet(name);
 
-  button.removeAttribute("disabled");
+// addMoneyButton.addEventListener("click", async (e)=>{
+//   e.preventDefault();
 
-  document.getElementById("greeting").innerText = greeting;
+//   const money_to_add = Number(addMoneyInput.value)
 
-  return false;
-});
+//   await dbank_backend.updateUserBalance(money_to_add)
+
+//   currentBalance = await dbank_backend.checkBalance();
+//   alert("Money Added success")
+// })
+
+document.querySelector("form").addEventListener("submit", async (e)=>{
+
+  e.preventDefault()
+
+  if(document.getElementById("add-money-input").value.length !== 0 || document.getElementById("withdraw-money-input").value.length !== 0){
+    
+      loading = true;
+
+      document.getElementById("loader").innerText = "loading..."
+
+      const addInput = parseFloat(document.getElementById("add-money-input").value);
+      const withdrawInput = parseFloat(document.getElementById("withdraw-money-input").value);
+      const button_ele = e.target.querySelector("#finalise-transaction-button");
+
+      button_ele.setAttribute("disbaled", true)
+
+      if(document.getElementById("add-money-input").value.length !== 0){
+        await dbank_backend.updateUserBalance(addInput)
+      }
+      
+      if(document.getElementById("withdraw-money-input").value.length !== 0){
+        await dbank_backend.withdrawUserBalance(withdrawInput)
+      }
+
+      const currentBalance = await dbank_backend.checkBalance();
+
+      loading = false
+      document.getElementById("loader").innerText = ""
+
+      button_ele.removeAttribute("disbaled")
+
+      document.getElementById("current-balance-amount").innerText = Math.round(currentBalance * 100) / 100;
+  }
+})
